d8789eaf91c78d4bbbfb0e0ee8d3567cfd6f6c09
Khenz00 <koflee.inc@gmail.com>
2022-04-29 02:56:26 +0300

    first commit

--- Homework2.py
+++ Homework2.py
@@ -1,15 +1,18 @@
 number_1= int(input('Введите первое число: '))
-input('Введите операцию')
+operation=input('Введите операцию')
 number_2= int(input('Введите второе число: '))
 
-#Сложение
-print(number_1+number_2)
+if operation == '+':
+ print(number_1+number_2)
 
-#Вычитание
-print(number_1-number_2)
+elif operation == '-':
+ print(number_1-number_2)
 
-#Умножение
-print(number_1*number_2)
+elif operation == '*':
+ print(number_1*number_2)
 
-#Деление
-print(number_1/number_2)
+elif operation == '/':
+ print(number_1/number_2)
+
+
+
