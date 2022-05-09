edbf3a9dcee36c682e632821b9f6be95953b9a97
Khenz00 <koflee.inc@gmail.com>
2022-05-10 02:29:57 +0300

    first commit

--- Homework2.py
+++ Homework2.py
@@ -1,18 +1,26 @@
-number_1= int(input('Введите первое число: '))
-operation=input('Введите операцию')
-number_2= int(input('Введите второе число: '))
-
-if operation == '+':
- print(number_1+number_2)
-
-elif operation == '-':
- print(number_1-number_2)
-
-elif operation == '*':
- print(number_1*number_2)
-
-elif operation == '/':
- print(number_1/number_2)
-
-
-
+def calc(number_1,number_2,operation):
+    if operation == '+':
+      print(number_1+number_2)
+    elif operation == '-':
+      print(number_1-number_2)
+    elif operation == '*':
+      print(number_1*number_2)
+    elif operation == '/':
+      print(number_1/number_2)
+    try:
+      print(number_1/number_2)
+    except ZeroDivisionError:
+      print('Деление на ноль!')
+    else:
+     print(number_1/number_2)
+number_1 = int(input('Введите первое число: '))
+operation = input('Введите операцию')
+number_2 = int(input('Введите второе число: '))
+count = 1
+while operation!='stop':
+       calc(number_1,number_2,operation)
+       if operation!= 'stop':
+           number_1 = int(input('Введите первое число: '))
+           number_2 = int(input('Введите второе число: '))
+       count+=1
+       print(f'было выполнено {count}')
