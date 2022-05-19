57e35ab544beebde1aefa06552cf0cfdd6cf94f6
Khenz00 <koflee.inc@gmail.com>
2022-05-20 02:49:58 +0300

    first commit

--- hw61.py
+++ hw61.py
@@ -1,19 +1,32 @@
 class Car:
-    global speed
-    speed=
+
+    speed=int
     model=str
     year_issue=int
     name=str
-    def __init__(self,speed=[]):
+    def __init__(self,speed=0):
         self.speed=speed
+    def __iter__(self):
+        return self
+    def __next__(self):
+        speed = self.speed
+        self.speed +=5
+        return speed
     def speed_up(self):
-        return (self.speed+5)
+        speed = self.speed
+        self.speed+=5
+        return speed
     def speed_down(self):
-        return self.speed-5
-    def stop(self):
-        return self.speed
+        speed=self.speed
+        self.speed-=5
+        return speed
     def speed_current(self):
-        return self.speed
+        speed=self.speed
+        return speed
+    def stop(self):
+        speed=self.speed
+        speed*=0
+        return speed
 
 
 
@@ -21,3 +34,7 @@ class Car:
 
 c=Car()
 print(c.speed_up())
+print(c.speed_up())
+print(c.speed_down())
+print(c.speed_current())
+print(c.stop())
