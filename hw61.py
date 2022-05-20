f73cd3c252fe7f87fe9d314a87a1df5529348113
Khenz00 <koflee.inc@gmail.com>
2022-05-20 16:09:41 +0300

    first commit

--- hw61.py
+++ hw61.py
@@ -1,40 +1,32 @@
 class Car:
 
-    speed=int
-    model=str
-    year_issue=int
-    name=str
-    def __init__(self,speed=0):
-        self.speed=speed
-    def __iter__(self):
-        return self
-    def __next__(self):
-        speed = self.speed
-        self.speed +=5
-        return speed
+    __speed=int
+    __model=str
+    __year_issue=int
+    __mark=str
+    def __init__(self,mark,year_issue,model,speed=0):
+        self.__speed=speed
+        self.__mark=mark
+        self.__model=model
+        self.__year_issue=year_issue
     def speed_up(self):
-        speed = self.speed
-        self.speed+=5
-        return speed
+        return self.__speed+5
     def speed_down(self):
-        speed=self.speed
-        self.speed-=5
-        return speed
+        return self.__speed-5
     def speed_current(self):
-        speed=self.speed
-        return speed
+        return self.__speed
     def stop(self):
-        speed=self.speed
-        speed*=0
-        return speed
+        return self.__speed-self.__speed
+    def reverse(self):
+        return self.__speed-self.__speed*2
 
 
 
 
 
-c=Car()
-print(c.speed_up())
+c=Car('Audi','1995','A5',20)
 print(c.speed_up())
 print(c.speed_down())
 print(c.speed_current())
 print(c.stop())
+print(c.reverse())
